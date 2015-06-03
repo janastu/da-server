5d76b24606f7aa2821b692f6db8e33b345b7e6dd
Arvind <arvindkhadri@gmail.com>
2015-06-03 16:59:08 +0530

    Add file size information in the response
    Add file size in the response for the API.

--- servers/server.py
+++ servers/server.py
@@ -10,7 +10,14 @@ app.config.from_pyfile('config.py')
 def index():
     """Get all files in a given directory"""
     uploaded_files = os.listdir(app.config.get('FILE_UPLOAD_FOLDER'))
-    return jsonify({'uploaded_files': uploaded_files})
+    upload_info = []
+    for upload in uploaded_files:
+        upload_item = []
+        upload_item.append(upload)
+        upload_item.append(float(os.path.getsize(os.path.join(
+            app.config.get('FILE_UPLOAD_FOLDER'), upload)))/1024)
+        upload_info.append(upload_item)
+    return jsonify({'uploaded_files': upload_info})
 
 
 if __name__ == "__main__":
