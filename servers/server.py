0b4c70e1e5de12f2eff3be8b731cdcefc41686eb
Arvind <arvindkhadri@gmail.com>
2015-07-01 12:51:35 +0530

    Fix API
    Fix tags API for a broken functionality. Flask converts list's key to
    have a [] at the end.

--- servers/server.py
+++ servers/server.py
@@ -66,14 +66,13 @@ def upload():
 def set_tags(id):
     """Update tags for a given file, create the tags if it is not present.
     id is the file id"""
-    print repr(request.form)
     if mongo.db.tags.find_one({'fileID': id}) is not None:
         mongo.db.tags.update({'fileID': id},
                              {"$set":
-                              {"tags": request.form.getlist('tags')}})
+                              {"tags": request.form.getlist('tags[]')}})
     else:
         mongo.db.tags.save({'fileID': id,
-                            'tags': request.form.getlist('tags')})
+                            'tags': request.form.getlist('tags[]')})
     return make_response()
 
 
